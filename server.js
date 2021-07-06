e6e88eb4b9deea5bf7f36a5f5ee27f13ac4098ee
DARKATOV <76508674+DARKATOV@users.noreply.github.com>
2021-07-06 23:34:20 +0200

    act

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const messages = [
+const messagesToTry = [
   { id: 0, from: "Bart", text: "Welcome to CYF chat system!" },
   { id: 1, from: "Anne", text: "Good to see you" },
   { id: 2, from: "Helen", text: "Long time, no see" },
@@ -22,95 +22,95 @@ const messages = [
   { id: 12, from: "Anne", text: "let's go for a coffee 4" }
 ];
 
-function getNextId() {
-  const lastMessageIndex = messages.length - 1;
-  if (lastMessageIndex === -1) {
-    return 0;
-  } else {
-    const nextId = lastMessageIndex + 1;
-    return nextId;
-  }
-  // for json object, the id has to be a string: nextId.toString();
-}
+const alphaMessage = { id: 0, from: "matrix", text: "Welcome to CYF chat system!" }
+let messages = [alphaMessage];
+// temp memory 
 
-function isValidMessage(message) {
-  if (message.text && message.from) {
-    return true;
-  }
-  return false;
-}
+//no use 
+// function getNextId() {
+//   const lastMessageIndex = messages.length - 1;
+//   if (lastMessageIndex === -1) {
+//     return 0;
+//   } else {
+//     const nextId = lastMessageIndex + 1;
+//     return nextId;
+//   }
+//   // for json object, the id has to be a string: nextId.toString();
+// }
+
+// function isValidMessage(message) {
+//   if (message.text && message.from) {
+//     return true;
+//   }
+//   return false;
+// }
 
 app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+  response.sendFile(__dirname + "/index.html"); // que es dirname
 });
 
-app.get("/messages", (req, res) => {
-  res.status(201).send(messages);
+app.get("/messages", (request, response) => {
+  response.status(201).send(messages);
 });
 
-
-app.get("/messages/search", (req, res) => {
-  // get search terms from query params
-  const searchTerm = req.query.text.toLowerCase()
-  // find messages containing the search term
-  const result = messages.filter(item => item.text.toLowerCase().includes(searchTerm))
-  // return them
-  res.send(result)
-})
-
-app.get("/messages/latest", (req, res) => {
-  // get the LAST 10 messages and return them
-  // slice with a negative number will create a new array with the items of the number, counting backwards
-  let result = messages.slice(-10)
-  res.send(result)
+app.get("/messages/search/:word", (request, response) => {
+  const searchTerm = request.params.word;
+  console.log(searchTerm);
+  const resultSearch = messages.filter(item => item.text.includes(searchTerm))
+  console.log(resultSearch);
+  response.status(200).send(resultSearch);
 });
 
+app.get("/messages/latest", (request, response) => {
+  // let resultSearch = messages.slice(-10) // slice con los ultimos diez elementos 
+  let resultSearch = messagesToTry.slice(-10) // slice con los ultimos diez elementos 
+  console.log(resultSearch);
+  response.send(resultSearch);
+});
 
-app.get("/messages/:id", (req, res) => {
-  const messageId = parseInt(req.params.id);
+app.get("/messages/:id", (request, response) => {
+  const messageId = parseInt(request.params.id);
   const message = messages.find((message) => message.id === messageId);
   if (message) {
-    res.status(201).send(message);
+    response.status(201).send(message);
   } else {
-    res.status(404).send("This message does not exist");
+    response.status(404).send("This message does not exist");
   }
 });
 
-
-
-app.post("/messages", (req, res) => {
+app.post("/messages", (request, response) => {
   const message = {
     id:  getNextId(),
-    from: req.body.from,
-    text: req.body.text,
+    from: request.body.from,
+    text: request.body.text,
   };
   // add timestamp to each new message
   message.timeSent = new Date()
 
   if (!isValidMessage(message)) {
-    res.status(404).send("This message is not complete.");
+    response.status(404).send("This message is not complete.");
     return;
   }
   messages.push(message);
-  res.status(201).send(message);
+  response.status(201).send(message);
 });
 
-app.put("/messages/:id", (req, res) => {
-  const messageId = parseInt(req.params.id);
-  let updatedMessage = req.body;
+app.put("/messages/:id", (request, response) => {
+  const messageId = parseInt(request.params.id);
+  let updatedMessage = request.body;
 
   let message = messages.find((message) => message.id === messageId);
   if (!message) {
-    res.status(404).send("This message does not exist");
+    response.status(404).send("This message does not exist");
   }
     message.from = updatedMessage.from;
     message.text = updatedMessage.text;
     timeSent = message.timeSent
-    res.status(201).send(updatedMessage);
+    response.status(201).send(updatedMessage);
 });
 
-app.delete("/messages/:id", (req, res) => {
-  const messageId = req.params.id;
+app.delete("/messages/:id", (request, response) => {
+  const messageId = request.params.id;
 
   const index = messages.findIndex((message) => message.id == messageId);
   // findIndex() returns -1 if item is not found, but -1 should not be used as index: negative number is not a valid position
